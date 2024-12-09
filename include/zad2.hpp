581dcfe2bbd8f11e2a397095116db951486a8616
CramY2001 <szymek.kramarczyk@gmail.com>
2024-12-10 00:51:21 +0100

    proba 1
    proba 1

--- include/zad2.hpp
+++ include/zad2.hpp
@@ -3,7 +3,18 @@
 #include <memory>
 #include <string>
 #include <type_traits>
-
+using namespace std;
 // tutaj klasa Penne
+class Penne : public Makaron
+{
+public:
+    double ileMaki(unsigned P) const { return P; }
+};
 
-// tutaj definicja metody gotujMakaron
+Makaron* Makaron::gotujMakaron(const string& s)
+{
+    if (s.front() == s.back())
+        return new Tagliatelle{3.14, .42, .1};
+    else
+        return new Penne;
+}
--- include/zad3.hpp
+++ include/zad3.hpp
@@ -1,5 +1,7 @@
 #include "zad1.hpp"
 #include <cstdint>
 #include <functional>
-
+using namespace std;
 // tutaj funkcja polejSosem
+template< typename T>
+size_t polejSosem(const Tagliatelle& t,const T& sos){return sos.polej(t);}
